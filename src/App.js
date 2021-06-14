e53a83be57ca0aedc8cf94115de3e87c47b9c073
Hamed Ghazali <hamedg20@yahoo.com>
2021-06-14 09:12:40 +0430

    routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,66 @@
+import { Fragment, useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
 import './App.css';
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+import Register from './components/Register';
 
-function App() {
-  return <div></div>;
-}
+const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const setAuth = (boolean) => {
+    setIsAuthenticated(boolean);
+  };
+
+  return (
+    <Fragment>
+      <Router>
+        <div className="container">
+          <Switch>
+            <Route
+              exact
+              path="/login"
+              render={(props) =>
+                !isAuthenticated ? (
+                  <Login {...props} setAuth={setAuth} />
+                ) : (
+                  <Redirect to="/dashboard" />
+                )
+              }
+            />
+            <Route
+              exact
+              path="/register"
+              render={(props) =>
+                !isAuthenticated ? (
+                  <Register {...props} setAuth={setAuth} />
+                ) : (
+                  <Redirect to="/login" />
+                )
+              }
+            />
+
+            <Route
+              exact
+              path="/dashboard"
+              render={(props) =>
+                isAuthenticated ? (
+                  <Dashboard {...props} setAuth={setAuth} />
+                ) : (
+                  <Redirect to="login" />
+                )
+              }
+            />
+          </Switch>
+        </div>
+      </Router>
+    </Fragment>
+  );
+};
 
 export default App;
---
+++ src/components/Register.jsx
@@ -0,0 +1,5 @@
+const Register = () => {
+  return <h1>this is register</h1>;
+};
+
+export default Register;
